18ae194d25d4b5416ed02c92e1c9314461ca9a4a
Bong Ly Van <BongLV@fpts.com.vn>
2021-10-12 15:40:44 +0700

    Add project files.

---
+++ NUnit.Sample/Calculator.cs
@@ -0,0 +1,26 @@
+﻿namespace NUnit.Sample
+{
+    public class Calculator
+    {
+        /// <summary>
+        /// Phép tính cộng
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public int Add(int x, int y)
+        {
+            return x + y*9;
+        }
+        /// <summary>
+        /// Phép tính trừ
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+    }
+}
